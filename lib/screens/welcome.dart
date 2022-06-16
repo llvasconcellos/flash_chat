b213d688bdff3c9bb368bfe82fc1bf3e3bce1d36
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2022-06-16 00:29:23 -0300

    More animations

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 import 'package:flash_chat/screens/login.dart';
 import 'package:flash_chat/screens/registration.dart';
@@ -12,11 +13,37 @@ class Welcome extends StatefulWidget {
   State<Welcome> createState() => _WelcomeState();
 }
 
-class _WelcomeState extends State<Welcome> {
+class _WelcomeState extends State<Welcome> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _backgroundAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _backgroundAnimation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(_animationController);
+    _animationController.addListener(() {
+      setState(() {});
+    });
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: _backgroundAnimation.value,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
@@ -32,13 +59,25 @@ class _WelcomeState extends State<Welcome> {
                     child: Image.asset('assets/images/logo.png'),
                   ),
                 ),
-                const Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.w900,
+                SizedBox(
+                  width: 250.0,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black54,
+                    ),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      animatedTexts: [
+                        TypewriterAnimatedText(
+                          'Flash Chat',
+                          speed: const Duration(milliseconds: 200),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                )
               ],
             ),
             const SizedBox(
